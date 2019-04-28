3fa62897e3c9cede0c5cce60c6b1ba59fc5b7807
fliiiix <hi@l33t.name>
2019-04-28 16:07:57 +0200

    Extend stream list with image

--- radiopi/api.py
+++ radiopi/api.py
@@ -39,14 +39,52 @@ class SleepTimerResource(Resource):
 
 class StreamUrlListResource(Resource):
     def render_GET(self, request):
-        streamlist = [{'name': 'Hardbase',         'url': 'http://listen.hardbase.fm/tunein-mp3-pls',   'orderid': 1},
-                      {'name': 'Technobase',       'url': 'http://listen.technobase.fm/tunein-mp3-asx', 'orderid': 2},
-                      {'name': 'Radio 24',         'url': 'http://icecast.radio24.ch/radio24',          'orderid': 0},
-                      {'name': 'Radio SRF 1',      'url': 'http://stream.srg-ssr.ch/m/drs1/mp3_128',    'orderid': 3},
-                      {'name': 'Radio SRF 2',      'url': 'http://stream.srg-ssr.ch/m/drs2/mp3_128',    'orderid': 4},
-                      {'name': 'Radio SRF 3',      'url': 'http://stream.srg-ssr.ch/m/drs3/mp3_128',    'orderid': 5},
-                      {'name': 'Radio Swiss Jazz', 'url': 'http://stream.srg-ssr.ch/m/rsj/mp3_128',     'orderid': 6},
-                      {'name': 'Radio Swiss Pop',  'url': 'http://stream.srg-ssr.ch/m/rsp/mp3_128',     'orderid': 7}]
+        streamlist = [{
+                        'name': 'Hardbase',
+                        'url': 'http://listen.hardbase.fm/tunein-mp3-pls',
+                        'img': '',
+                        'orderid': 1
+                      },
+                      {
+                        'name': 'Technobase',
+                        'url': 'http://listen.technobase.fm/tunein-mp3-asx',
+                        'img': '',
+                        'orderid': 2
+                      },
+                      {
+                        'name': 'Radio 24',
+                        'url': 'http://icecast.radio24.ch/radio24',
+                        'img': 'https://upload.wikimedia.org/wikipedia/de/thumb/3/33/Radio_24_Logo.svg/154px-Radio_24_Logo.svg.png',
+                        'orderid': 0
+                       },
+                      {
+                        'name': 'Radio SRF 1',
+                        'url': 'http://stream.srg-ssr.ch/m/drs1/mp3_128',
+                        'img': '',
+                        'orderid': 3
+                      },
+                      {
+                        'name': 'Radio SRF 2',
+                        'url': 'http://stream.srg-ssr.ch/m/drs2/mp3_128',
+                        'img': '',
+                        'orderid': 4
+                      },
+                      {
+                        'name': 'Radio SRF 3',
+                        'url': 'http://stream.srg-ssr.ch/m/drs3/mp3_128',
+                        'img': '',
+                        'orderid': 5},
+                      {
+                        'name': 'Radio Swiss Jazz',
+                        'url': 'http://stream.srg-ssr.ch/m/rsj/mp3_128',
+                        'img': '',
+                        'orderid': 6},
+                      {
+                        'name': 'Radio Swiss Pop',
+                        'url': 'http://stream.srg-ssr.ch/m/rsp/mp3_128',
+                        'img': '',
+                        'orderid': 7
+                      }]
 
         return json.dumps(streamlist).encode('utf8')
 
